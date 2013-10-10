316b18db2c7308ca01b23718e8b78da286b93927
Keven Boell <keven.boell@intel.com>
2013-10-10 13:33:28 +0200

    Print status of a VLA in Fortran during type print.
    If one executes e.g. print &vla, GDB will print the
    bounds in the following way if not allocated:
      (0:0, 0:0, ...)
    This fix will change this behaviour to:
      (<not allocated>)
    The same applies for associated.
    
    2013-10-10  Keven Boell  <keven.boell@intel.com>
    
    	* f-typeprint.c (f_type_print_varspec_suffix): Add
    	support for allocated/associated tag.
    
    Change-Id: I41bfa7446e6a599bdb0c3e88252734216e610d4b
    Signed-off-by: Keven Boell <keven.boell@intel.com>

--- gdb/f-typeprint.c
+++ gdb/f-typeprint.c
@@ -182,28 +182,35 @@ f_type_print_varspec_suffix (struct type *type, struct ui_file *stream,
       if (arrayprint_recurse_level == 1)
 	fprintf_filtered (stream, "(");
 
-      if (TYPE_CODE (TYPE_TARGET_TYPE (type)) == TYPE_CODE_ARRAY)
-	f_type_print_varspec_suffix (TYPE_TARGET_TYPE (type), stream, 0, 0, 0,
-				     arrayprint_recurse_level);
-
-      lower_bound = f77_get_lowerbound (type);
-      if (lower_bound != 1)	/* Not the default.  */
-	fprintf_filtered (stream, "%d:", lower_bound);
-
-      /* Make sure that, if we have an assumed size array, we
-         print out a warning and print the upperbound as '*'.  */
-
-      if (TYPE_ARRAY_UPPER_BOUND_IS_UNDEFINED (type))
-	fprintf_filtered (stream, "*");
+      if (!TYPE_ALLOCATED_PROP (type) && TYPE_NOT_ALLOCATED (type))
+        val_print_not_allocated (stream);
+      else if (!TYPE_ASSOCIATED_PROP (type) && TYPE_NOT_ASSOCIATED (type))
+        val_print_not_associated (stream);
       else
-	{
-	  upper_bound = f77_get_upperbound (type);
-	  fprintf_filtered (stream, "%d", upper_bound);
-	}
+        {
+          if (TYPE_CODE (TYPE_TARGET_TYPE (type)) == TYPE_CODE_ARRAY)
+            f_type_print_varspec_suffix (TYPE_TARGET_TYPE (type), stream, 0, 0, 0,
+                 arrayprint_recurse_level);
 
-      if (TYPE_CODE (TYPE_TARGET_TYPE (type)) != TYPE_CODE_ARRAY)
-	f_type_print_varspec_suffix (TYPE_TARGET_TYPE (type), stream, 0, 0, 0,
-				     arrayprint_recurse_level);
+          lower_bound = f77_get_lowerbound (type);
+          if (lower_bound != 1)	/* Not the default.  */
+            fprintf_filtered (stream, "%d:", lower_bound);
+
+          /* Make sure that, if we have an assumed size array, we
+             print out a warning and print the upperbound as '*'.  */
+
+          if (TYPE_ARRAY_UPPER_BOUND_IS_UNDEFINED (type))
+            fprintf_filtered (stream, "*");
+          else
+            {
+              upper_bound = f77_get_upperbound (type);
+              fprintf_filtered (stream, "%d", upper_bound);
+            }
+
+          if (TYPE_CODE (TYPE_TARGET_TYPE (type)) != TYPE_CODE_ARRAY)
+            f_type_print_varspec_suffix (TYPE_TARGET_TYPE (type), stream, 0, 0, 0,
+                 arrayprint_recurse_level);
+        }
       if (arrayprint_recurse_level == 1)
 	fprintf_filtered (stream, ")");
       else
